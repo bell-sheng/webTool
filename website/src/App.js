ea05c69a7507a7b878ded0b324e93c11bba151c8
bellzhong <972242015@qq.com>
2023-07-22 08:36:49 +0800

    解决编译打包问题

--- website/src/App.js
+++ website/src/App.js
@@ -1,19 +1,8 @@
 import React, {memo} from "react";
 
-import {HashRouter} from "react-router-dom";
-
-import "./assets/css/base.css"
-import HomeHeader from "./components/home-header";
-import HomeFooter from "./components/home-footer";
-import Router from "./router";
-
 function App() {
     return (
-        <HashRouter>
-            <HomeHeader/>
-            <Router/>
-            <HomeFooter/>
-        </HashRouter>
+        <div>Test</div>
     );
 }
 
