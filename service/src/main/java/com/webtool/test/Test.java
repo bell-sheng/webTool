78f521f50c98806e70cbd837939fbd0ebd8bea48
bellzhong <972242015@qq.com>
2023-11-08 07:40:56 +0800

    前端路由组合

---
+++ service/src/main/java/com/webtool/test/Test.java
@@ -0,0 +1,9 @@
+package com.webtool.test;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.printf("Test");
+    }
+}
--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,20 @@
 import React, {memo} from "react";
 
+import {HashRouter} from "react-router-dom";
+
+import "./assets/css/base.css"
+import HomeHeader from "./components/home-header";
+import HomeFooter from "./components/home-footer";
+import Router from "./router";
+
 function App() {
     return (
-        <div>Test</div>
-    );
+        <HashRouter>
+            <HomeHeader/>
+            <Router/>
+            <HomeFooter/>
+        </HashRouter>
+);
 }
 
 export default memo(App);
